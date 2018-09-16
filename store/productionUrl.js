aeb5b57843e178521ce3d26f896b19280ad5a278
Jeremy Grubard <jgrubard@gmail.com>
2018-09-16 12:49:13 -0400

    change production url

--- store/productionUrl.js
+++ store/productionUrl.js
@@ -1,5 +1,6 @@
 let localHost;
-localHost = 'http://192.168.1.175:3000';
 // localHost = 'http://localhost:3000';
-const url = localHost || 'http://immense-escarpment-58025.herokuapp.com';
+// const url = localHost || 'http://immense-escarpment-58025.herokuapp.com'; // team deployment
+const url = localHost || 'https://pair-up.herokuapp.com';
+
 export default url;
